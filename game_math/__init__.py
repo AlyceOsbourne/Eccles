1fa0d2a669177c6f4b9ae9aabe6dd41b80f833a1
Alyce <Alyceosbourne@outlook.com>
2022-01-23 20:03:05 +0000

    Adding Component stubs to be fleshed out later

--- game_math/__init__.py
+++ game_math/__init__.py
@@ -1,121 +1,15 @@
 # module for common math functions
-from abc import abstractmethod
-from collections import namedtuple
+import logging
 from dataclasses import dataclass, field
-
 import math
-from functools import cached_property, cache
-from math import atan2
 
-
-class __V:
-    @abstractmethod
-    def magnitude(self):
-        pass
-
-    @abstractmethod
-    def angle(self, other):
-        pass
-
-    @abstractmethod
-    def convert_to_unit(self):
-        pass
-
-    @abstractmethod
-    def __add__(self, other):
-        pass
-
-
-class __P:
-    @abstractmethod
-    def __add__(self, other):
-        pass
-
+math_logger = logging.Logger(__name__)
 
 @dataclass(eq=True, slots=True, init=True)
-class Point2(__P):
-    x: int = field(default=0)
-    y: int = field(default=0)
-
-    @property
-    def pos(self):
-        return self.x, self.y
-
-    @pos.setter
-    def pos(self, pos):
-        self.x, self.y = pos
-
-    def __add__(self, other):
-        self.x += other.x
-        self.y += other.y
-
-
-@dataclass(eq=True, slots=True, init=True)
-class Point3(__P):
-    x: int = field(default=0)
-    y: int = field(default=0)
-    z: int = field(default=0)
-
-    @property
-    def pos(self):
-        return self.x, self.y, self.z
-
-    @pos.setter
-    def pos(self, pos):
-        self.x, self.y, self.z = pos
-
-    def __add__(self, other):
-        self.x += other.x
-        self.y += other.y
-        self.z += other.z
-
-
-@dataclass(slots=True, eq=True, init=True)
-class Vector2(__V):
-    x: int or float
-    y: int or float
-
-    @property
-    def vec(self):
-        return self.x, self.y
-
-    @vec.setter
-    def vec(self, vec):
-        self.x, self.y = vec
-
-    def __add__(self, other):
-        self.x += other.x
-        self.y += other.y
-        return self
-
-    def convert_to_unit(self):
-        mag = self.magnitude
-        self.x /= mag
-        self.y /= mag
-
-    @cached_property
-    def magnitude(self):
-        return math.sqrt((self.x * self.x) + (self.y * self.y))
-
-    def angle(self, other):
-        return atan2(other.y - self.y, other.x - self.x) * (180 / math.pi)
-
-
-@dataclass(slots=True, eq=True, init=True)
-class Vector3(__V):
-    x: int or float = field(default=0.)
-    y: int or float = field(default=0.)
-    z: int or float = field(default=0.)
-
-    def __add__(self, other):
-        self.x += other.x
-        self.y += other.y
-        self.z += other.z
-        return self
-
-    @cached_property
-    def magnitude(self):
-        return math.sqrt((self.x * self.x) + (self.y * self.y) + (self.z * self.z))
+class Vector:
+    _x: float = field(default=0.)
+    _y: float = field(default=0.)
+    _z: float = field(default=0.)
 
     @property
     def vec(self):
@@ -125,22 +19,87 @@ class Vector3(__V):
     def vec(self, vec):
         self.x, self.y, self.z = vec
 
-    def angle(self, other):
-        a = self.magnitude
-        b = other.magnitude
-        ab = a + b
-        as2 = math.sqrt(a)
-        bs2 = math.sqrt(b)
-        return math.acos(ab / (as2 * bs2))
+    @vec.setter
+    def vec(self, x=0., y=0., z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    @property
+    def magnitude(self):
+        return math.hypot(self.x, self.y, self.z)
+
+    @magnitude.setter
+    def magnitude(self, magnitude):
+        self.convert_to_unit()
+        self.vec = tuple((v * magnitude for v in self.vec))
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        self._x = x
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, y):
+        self._y = y
+
+    @property
+    def z(self):
+        return self._z
+
+    @z.setter
+    def z(self, z):
+        self._z = z
+
+    @property
+    def angle(self):
+        if self.z == 0:
+            return self.y/self.magnitude
+        else:
+            return self.y/self.magnitude, self.z/self.magnitude
 
     def convert_to_unit(self):
-        self.x /= self.magnitude
-        self.y /= self.magnitude
-        self.z /= self.magnitude
+        m = self.magnitude
+        return self.__create__(*(v/m for v in self.vec))
+
+    def __add__(self, other):
+        x = other.x + self.x
+        y = other.y + self.y
+        z = other.z + self.z
+        return self.__create__(x, y, z)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(x={self.x}, y={self.y}, z={self.z})'
+
+    def __str__(self):
+        out = f'{self.__class__.__name__}{2 if self.z == 0 else 3}D''\n\r'f'-> x = {self.x}''\n\r'f'-> y = {self.y}'
+        if self.z > 0:
+            out += '\n\r'f'-> z = {self.z}'
+
+        out += '\n\r'f'-> angle {self.angle}'
+        out += '\n\r'f'-> magnitude {self.magnitude}'
+        out += '\n\r'
+        return out
+
+    @classmethod
+    def __create__(cls, x, y, z):
+        return cls(x, y, z)
 
 
-vec_a = Vector3(-5, 5, 2)
-vec_b = Vector3(10, 10, 5)
+v1 = Vector(10., 15., 20)
+v2 = Vector(10., 20.)
+v3 = Vector(10. -70.)
+v = (v1+v2+v3).convert_to_unit()
 
-print(vec_a + vec_b)
-print(vec_a.angle(vec_b))
+print(v1)
+print(v2)
+print(v3)
+print(v)
+
