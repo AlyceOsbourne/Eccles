1a04e352e8716f7cf25491e16eab96b4729eadf8
Alyce <Alyceosbourne@outlook.com>
2022-02-06 14:24:59 +0000

    Docs look good
    Signed-off-by: Alyce <Alyceosbourne@outlook.com>

--- common/__init__.py
+++ common/__init__.py
@@ -6,7 +6,7 @@ __VERSION__ = {'MAJOR': 0, 'MINOR': 0, 'DEBUG': 0, 'DEVELOPMENT': 3}
 __PROJECT_NAME__ = "Eccles"  # subject to change
 __VERSION_STR__ = "v{}.{}.{}.{}".format(*__VERSION__.values())
 
-__doc__ = """### Common ###
+__doc__ = """## Common ##
 
  Module to help cut down on code repetition, 
  if you find yourself repeating lines over and over 
